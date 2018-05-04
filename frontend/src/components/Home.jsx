6338b24dc6181b1612a9dd1addd02ce13aa01676
Erkki-Ilmari Rajakoski <erkki@studiorajakoski.com>
2018-05-05 00:17:59 +0300

    Home css added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
+import '../styling/Home.css';
 import fridge from '../images/fridge.svg';
 
 const Home = props => (
-    <div id="home">
-        <h1>Fridge poetry</h1>
-        <img src={fridge} alt="Fridge" />
-    </div>
+  <div id="home">
+    <h1>Fridge poetry</h1>
+    <img src={fridge} alt="Fridge" />
+  </div>
 );
 
 export default Home;
