acaee1792355a0a007797af57b6bf668e4552244
Erkki-Ilmari Rajakoski <erkki@studiorajakoski.com>
2018-05-06 19:59:16 +0300

    Dummy test added

--- frontend/src/tests/Word.test.js
+++ frontend/src/tests/Word.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Word from '../components/Word';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Word />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
