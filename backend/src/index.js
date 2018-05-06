a499a131d9fa93824b4ac6ad6724ba625ed9657f
Erkki-Ilmari Rajakoski <erkki@studiorajakoski.com>
2018-05-06 15:54:23 +0300

    Allow requests from all origins

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,13 @@
 const Koa = require('koa');
+const cors = require('koa-cors');
 const wordRoutes = require('./routes/word');
 
 const app = new Koa();
 const PORT = process.env.PORT || 9000;
+
+app.use(cors({
+  origin: '*'
+}));
 
 app.use(wordRoutes.routes());
 
